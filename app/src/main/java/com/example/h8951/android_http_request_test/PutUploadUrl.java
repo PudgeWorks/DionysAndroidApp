925bf74ee66e29d99560ae69c8d8b8633514ce26
Aleksi Tommila <aleksi.tommila@gmail.com>
2016-12-01 14:55:33 +0200

    Eurybus implemented PUT method

---
+++ app/src/main/java/com/example/h8951/android_http_request_test/PutUploadUrl.java
@@ -0,0 +1,92 @@
+package com.example.h8951.android_http_request_test;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+/**
+ * Created by Aleksi on 12.11.2016.
+ */
+
+public class PutUploadUrl {
+
+    // Given a URL, establishes an HttpUrlConnection and retrieves
+    // the web page content as a InputStream, which it returns as
+    // a string.
+
+    public void uploadUrl(String myurl, String nick, String nick_actual, String key, String value) throws IOException {
+        InputStream is = null;
+        String result = "";
+        // Only display the first 1500 characters of the retrieved
+        // web page content.
+        int len = 1500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(15000);
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setRequestMethod("PUT");
+
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setRequestProperty("Content-type", "application/json");
+
+            String json ="";
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put(nick,nick_actual);
+            jsonObject.put(key,value);
+            json = jsonObject.toString();
+
+            OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+            out.write(json);
+            out.close();
+            connection.getInputStream();
+
+            //connection.
+            /*HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;*/
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } catch (MalformedURLException ex) {
+            ex.printStackTrace();
+        } catch (ProtocolException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/h8951/android_http_request_test/UploadUrl.java
@@ -0,0 +1,105 @@
+package com.example.h8951.android_http_request_test;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by Aleksi on 12.11.2016.
+ */
+
+public class UploadUrl {
+
+    // Given a URL, establishes an HttpUrlConnection and retrieves
+    // the web page content as a InputStream, which it returns as
+    // a string.
+
+    public String uploadUrl(String myurl, String key, String value) throws IOException {
+        HttpURLConnection conn = null;
+        OutputStream outputPost;
+        String response = "";
+        try {
+            URL url = new URL(myurl);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty(key,value);
+            conn.setDoOutput(true);
+            // Starts the query
+            conn.connect();
+
+
+            outputPost = new BufferedOutputStream(conn.getOutputStream());
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(outputPost,"UTF-8"));
+            //writer.write(getPostDataString(postDataParams));
+            writer.flush();
+            writer.close();
+            outputPost.close();
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line=br.readLine()) != null) {
+                    response+=line;
+                }
+            }
+            else {
+                response="";
+
+            }
+
+
+        }
+        catch (MalformedURLException ex){
+
+        }
+        catch (SocketTimeoutException ex){
+
+        }
+        catch (IOException ex){
+
+        }
+        finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+        return  "ebin";
+    }
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException{
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
+    }
+
+}
